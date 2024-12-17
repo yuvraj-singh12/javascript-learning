c0e6c84a3c244ca56e82d95405c9c8db75e4bc5e
yuvraj-singh12 <89958823+yuvraj-singh12@users.noreply.github.com>
2024-12-17 13:31:27 +0000

    string basics

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -14,5 +14,17 @@ symbol **to find uniqueness**
 object
 */
 
-console.log("2" > 1);
-console.log("a3" > 1);
+// console.log("2" > 1);
+// console.log("a3" > 1);
+
+//stack(primitive) and heap(non-primitive)
+//stack give copy and heap give refrence of original value
+
+let myname = "Yuvraj"
+let anothername = myname
+
+myname = 'Singh'
+console.log(anothername);
+console.log(myname);
+
+
---
+++ 01_basics/03_string.js
@@ -0,0 +1,18 @@
+// to convert string into array we can use split method
+let str = " I am a string "
+
+let arr = str.trim().split('-',2)
+
+console.log(arr)
+
+console.log("hello".indexOf("n"));
+
+//join adds array items to string
+
+let newstr = arr.join('')
+console.log(newstr);
+
+console.log("5".padEnd(2,"0"))
+
+
+

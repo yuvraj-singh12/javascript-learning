5612f0f4a5eff1343d40a30505847948f601da81
yuvraj-singh12 <89958823+yuvraj-singh12@users.noreply.github.com>
2024-12-17 12:44:29 +0000

    bascis of js

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+
+let accountEmail ="email@email.com"
+
+var accountPassword = " account1234"
+
+city = "Jaipur"
+
+console.table([accountId, accountEmail, accountPassword, city])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // to use latest javascript version
+
+/*
+number
+bigint
+string
+boolean
+null
+undefined
+symbol **to find uniqueness**
+*/
+
+/*
+object
+*/
+
+console.log("2" > 1);
+console.log("a2" > 1);
